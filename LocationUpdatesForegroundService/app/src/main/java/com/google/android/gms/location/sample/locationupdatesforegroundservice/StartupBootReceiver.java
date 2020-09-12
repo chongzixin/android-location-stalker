8e114d69c70f2592af2c3defa2f2896849ba715f
zixin <czixin@gmail.com>
2020-09-12 19:54:08 +0800

    feat: restart service automatically on phone reboot

---
+++ LocationUpdatesForegroundService/app/src/main/java/com/google/android/gms/location/sample/locationupdatesforegroundservice/StartupBootReceiver.java
@@ -0,0 +1,21 @@
+package com.google.android.gms.location.sample.locationupdatesforegroundservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+public class StartupBootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("StartupBootReceiver", "boot completed from com.google.android.gms.location.sample.locationupdatesforegroundservice");
+
+        Intent serviceIntent = new Intent(context, LocationUpdatesService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
+    }
+}
